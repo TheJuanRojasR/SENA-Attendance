23a13c8ba7b620793f4bd19b47ee97bd2855ce9c
LauraVOrtizR <laura_vortizr@soy.sena.edu.co>
2026-08-26 18:44:44 -0500

    style(footer): Added footer content

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -2,15 +2,48 @@ import './footer.scss';
 
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
-import { Translate } from 'react-jhipster';
+
+export const BrandIcon = props => (
+  <div {...props} className="brand-icon">
+    <img src="content/images/logo.png" alt="Logo" />
+  </div>
+);
 
 const Footer = () => (
   <div className="footer page-content">
-    <Row>
+    <Row className="footer-branding">
       <Col md="12">
-        <p>
-          <Translate contentKey="footer">Your footer</Translate>
-        </p>
+        <BrandIcon />
+        <span className="footer-brand-title">
+          <span> SENA </span>
+          <span> Attendance </span>
+        </span>
+      </Col>
+      <Col md="12">
+        <span className="footer-text">
+          <span>© 2024 SENA Attendance.</span>
+          <span>Todos los derechos reservados.</span>
+        </span>
+      </Col>
+      <Col md="12">
+        <span className="footer-version">Version 1.0.0</span>
+      </Col>
+    </Row>
+    <Row className="footer-branding">
+      <Col>
+        <span className="footer-links">
+          <span>Politica de Privacidad</span>
+        </span>
+      </Col>
+      <Col>
+        <span className="footer-links">
+          <span>Términos y Condiciones</span>
+        </span>
+      </Col>
+      <Col>
+        <span className="footer-links">
+          <span>Contacto</span>
+        </span>
       </Col>
     </Row>
   </div>
