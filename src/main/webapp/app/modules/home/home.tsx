abbb0d71b5e2ee4e74876d448c8f0fc69410c015
LauraVOrtizR <laura_vortizr@soy.sena.edu.co>
2026-08-26 19:00:53 -0500

    feat(ladingPage): Creation of content for landing page)

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,59 +1,55 @@
 import './home.scss';
 
 import React from 'react';
-import { Alert, Col, Row } from 'react-bootstrap';
+import { Button, Card, Col, Row } from 'react-bootstrap';
 import { Translate } from 'react-jhipster';
 import { Link } from 'react-router';
 
-import { useAppSelector } from 'app/config/store';
+import { faListCheck } from '@fortawesome/free-solid-svg-icons';
+import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
+import { faChartLine } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Home = () => {
-  const account = useAppSelector(state => state.authentication.account);
-
   return (
     <Row>
-      <Col md="3" className="pad">
-        <span className="hipster rounded" />
-      </Col>
-      <Col md="9">
+      <Col md="7" className="d-flex flex-column justify-content-evenly">
         <h1 className="display-4">
-          <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
+          <Translate contentKey="home.title">Optimization of Academic Management and Attendance</Translate>
         </h1>
         <p className="lead">
-          <Translate contentKey="home.subtitle">This is your homepage</Translate>
+          <Translate contentKey="home.subtitle">
+            An institutional platform designed to facilitate monitoring and tracking. Precise tools for instructors, trainees, and
+            administrative staff that ensure transparency and efficiency in SENA's processes.
+          </Translate>
         </p>
-        {account?.login ? (
-          <div>
-            <Alert variant="success">
-              <Translate contentKey="home.logged.message" interpolate={{ username: account.login }}>
-                You are logged in as user {account.login}.
-              </Translate>
-            </Alert>
-          </div>
-        ) : (
-          <div>
-            <Alert variant="success">
-              <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
-
-              <Link to="/login" className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-              </Link>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
-                , you can try the default accounts:
-                <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-                <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-              </Translate>
-            </Alert>
-
-            <Alert variant="success">
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
-              <Link to="/account/register" className="alert-link">
-                <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
-              </Link>
-              <Translate contentKey="global.messages.info.register.suffix" />
-            </Alert>
-          </div>
-        )}
+        <Button as={Link as any} to="/login" size="lg" color="primary" className="button-generic">
+          <Translate contentKey="home.button">Access the platform</Translate>
+        </Button>
+      </Col>
+      <Col md="5" className="pad">
+        <span className="hipster rounded" />
+      </Col>
+      <Col md="12" className="pad home-content">
+        <h5>Capacidades Centrales</h5>
+        <p>Herramientas robustas diseñadas para optimizar la gestión académica y el control de asistencia en SENA.</p>
+      </Col>
+      <Col md="12" className="pad home-cards">
+        <Card className="home-card">
+          <FontAwesomeIcon className="home-icon" icon={faListCheck} size="2x" />
+          <h6>Seguimiento de Asistencia</h6>
+          <p>Registro digital y centralizado para control diario por ficha y competencia, asegurando trazabilidad completa.</p>
+        </Card>
+        <Card className="home-card">
+          <FontAwesomeIcon className="home-icon" icon={faFileAlt} size="2x" />
+          <h6> Gestion de Justificaciones </h6>
+          <p>Flujo automatizado para la radicación y aprobación de excusas médicas e inasistencias con soporte documental.</p>
+        </Card>
+        <Card className="home-card">
+          <FontAwesomeIcon className="home-icon" icon={faChartLine} size="2x" />
+          <h6> Reportes en Tiempo Real </h6>
+          <p>Generación de indicadores de deserción y ausentismo para toma de decisiones oportunas en coordinación.</p>
+        </Card>
       </Col>
     </Row>
   );
